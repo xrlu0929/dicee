b1704602c718f559d19e9c769aa97e347ad2194b
xrlu0929 <xl2474@tc.columbia.edu>
2018-07-20 11:03:19 -0400

    test local location changes

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ViewController: UIViewController {
     var randomDiceIndex1 : Int = 0;
     var randomDiceIndex2 : Int = 0;
-    
     let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     
     @IBOutlet weak var diceImageView1: UIImageView!
